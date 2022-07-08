914adc10cd06326a1c54765c1f362a1b7dc94efb
tizxr <57099237+faiqff@users.noreply.github.com>
2022-07-08 11:14:21 +0100

    Update main.py
    Added Cooldowns, Smaller Code.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 from time import sleep
 from colorama import Fore
+import random
 import os
-from requests_html import HTML, HTMLSession
+import threading
+
+
 
 os.system("cls")
 print(Fore.RED + """
@@ -17,18 +20,16 @@ print(Fore.RED + """
 Made by tizxr#3313 | Free Version.
     """)
 print(Fore.RESET)
-shirtlink = input("Shirt That You Would Like To Bot (LINK): ")
-yourcookie = input("Your cookie that you would like to buy the shirt with")
+yourcookie = input("Your cookie that you would like to buy the shirt with: ")
 
-### Getting The Shit About Price And Stuff ###
-session = HTMLSession()
-r = session.get(f"{shirtlink}")
-r1 = r.html.find("#item-container")[0].attrs
-id = r1.get("data-product-id")
-item_id = r1.get("data-item-id")
-info = requests.get(f"https://api.roblox.com/marketplace/productinfo?assetId={item_id}").json()
+shirtlink = input("Shirt That You Would Like To Bot (ID OF THE): ")
+
+
+
+info = requests.get(f"https://api.roblox.com/marketplace/productinfo?assetId={shirtlink}").json()
 creator_id = info["Creator"].get("Id")
 amount = info["PriceInRobux"]
+id = info["ProductId"]
 ###
 
 ### Getting YOUR robux and stuff ###
@@ -44,16 +45,38 @@ else:
     while True:
         sleep(1)
 
+def token():
+    cookie = {".ROBLOSECURITY": yourcookie}
+    check = requests.get("https://users.roblox.com/v1/users/authenticated", cookies=cookie)
+    if check.status_code==200:
+        token = requests.post('https://auth.roblox.com/v2/logout', cookies=cookie).headers["x-csrf-token"]
+        return token
+    else:
+        return False
+
 ### Buying loop ###
-while True:
-    sleep(1)
+
+loop = True
+while loop:
     link = f"https://economy.roblox.com/v1/purchases/products/{id}"
     buy = requests.post(link, cookies=cookie, headers=header, json={"expectedCurrency": "1", "expectedPrice": f"{amount}", "expectedSellerId": f"{creator_id}"})
-    delete = requests.post("https://www.roblox.com/asset/delete-from-inventory", cookies=cookie, headers=header, json={"assetId": f"{item_id}"})
+    delete = requests.post("https://www.roblox.com/asset/delete-from-inventory", cookies=cookie, headers=header, json={"assetId": f"{id}"})
     if buy.status_code==200 and delete.status_code==200:
-        print(Fore.GREEN + f"Shirt Bought | Status Code: {buy.status_code}")
+        print(Fore.GREEN + f"Shirt Bought | Status Code: 200")
+    elif buy.status_code==401:
+        loop = False
+        print("Wrong Cookie")
+    elif buy.status_code==403:
+        x = token()
+        if x==False:
+            print("Wrong Cookie")
+        else:
+            header = {'x-csrf-token': str(token()), 'content-type': 'application/json'}
+            loop = True
+    elif buy.status_code==429:
+        loop = False
+        print("Sleeping  5 secs, somehow meant to stop ratelimiteds")
+        sleep(5)
+        loop = True
     else:
-        print(buy.json())
-        print(Fore.GREEN + f"Something Went Wrong| Status Code: {buy.status_code}")
-        
-
+        pass
